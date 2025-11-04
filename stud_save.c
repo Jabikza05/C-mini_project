af4ec32321278ca1a1bbd43f41412e4cd05b3060
Jabikza Jaya Swvan R <157294475+Jabikza05@users.noreply.github.com>
2025-11-04 22:20:53 +0530

    Create stud_save.c

---
+++ stud_save.c
@@ -0,0 +1,52 @@
+#include"header.h"
+
+void stud_save(ST *p)
+{
+    FILE *fp;
+    char ch;
+
+    printf("\n_____________________________\n");
+    printf("|                             |\n");
+    printf("| S/s : save and exit         |\n");
+    printf("| E/e : exit without saving   |\n");
+    printf("|                             |\n");
+    printf("-------------------------------\n");
+
+    scanf(" %c", &ch);
+
+    switch(ch)
+    {
+        case 'S':
+        case 's':
+            fp = fopen("student.dat", "w");  // open once in write mode
+            if(fp == 0)
+            {
+                printf("File open failed...\n");
+                return;
+            }
+
+            while(p != 0)  // write all nodes
+            {
+                fprintf(fp, "%d %s %.2f\n", p->roll, p->name, p->marks);
+                p = p->next;
+            }
+
+            fclose(fp);
+            printf("All records saved successfully into student.dat\n");
+            exit(0);
+            break;
+
+        case 'E':
+        case 'e':
+            printf("Exit without saving...\n");
+            exit(0);
+            break;
+
+        default:
+            printf("Invalid choice...\n");
+            break;
+    }
+}
+
+
+
