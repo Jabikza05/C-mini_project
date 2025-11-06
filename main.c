959230818dbd5cbde9a19172f062fa9841c3d727
Jabikza Jaya Swvan R <157294475+Jabikza05@users.noreply.github.com>
2025-11-06 20:13:26 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,91 +1,91 @@
+
 #include"header.h"
 
 int main()
 {
+    ST* hptr = 0;
+    char op;
 
-   ST*hptr=0;
-   char op;
-
-do
-{       
-    printf("\n_________________________________________\n");
-      printf("|                                       |\n");
-      printf("|   *****STUDENT RECORD MENU*****       |\n");
-      printf("|   ----------------------------------  |\n");
-      printf("|                                       |\n");
-      printf("|  a/A :add new record                  |\n");
-      printf("|  d/D :delete a record                 |\n");
-      printf("|  s/S :show the list                   |\n");
-      printf("|  m/M :modify a record                 |\n");
-      printf("|  v/V :save                            |\n");
-      printf("|  e/E :exit                            |\n");
-      printf("|  t/T :sort the list                   |\n");
-      printf("|  l/L :delete all the record           |\n");
-      printf("|  r/R :reverse the list     	        |\n");
-      printf("|                                       |\n");
-      printf("|                                       |\n");
-      printf("|  Enter your choice:                   |\n");
-      printf("-----------------------------------------\n");
-  
-   scanf(" %c",&op);
+    do
+    {       
+        printf("\n_________________________________________\n");
+        printf("|                                       |\n");
+        printf("|   *****STUDENT RECORD MENU*****       |\n");
+        printf("|   ----------------------------------  |\n");
+        printf("|                                       |\n");
+        printf("|  a/A :add new record                  |\n");
+        printf("|  d/D :delete a record                 |\n");
+        printf("|  s/S :show the list                   |\n");
+        printf("|  m/M :modify a record                 |\n");
+        printf("|  v/V :save                            |\n");
+        printf("|  e/E :exit                            |\n");
+        printf("|  t/T :sort the list                   |\n");
+        printf("|  l/L :delete all the record           |\n");
+        printf("|  r/R :reverse the list     	        |\n");
+        printf("|                                       |\n");
+        printf("|                                       |\n");
+        printf("-----------------------------------------\n");
  
- 
+        printf("Enter your choice:");
+        scanf(" %c",&op);
+        switch(op)
+        {
+            case 'a':
+            case 'A':
+                do
+                {   
+                    stud_add(&hptr);  
+                    printf("Do you want to enter the student data again:[y/Y]");
+                    scanf(" %c",&op);
+                } while(op=='y'||op=='Y');
+                break;
 
-  switch(op)
-  {
-    case 'a':
-    case 'A':   
-        stud_add(&hptr);  
-        break;
+            case 'd':
+            case 'D':
+                stud_del(&hptr);
+                break;
 
-    case 'd':
-    case 'D':
-        stud_del(&hptr);
-        break;
+            case 's':
+            case 'S':
+                stud_show(hptr);
+                break;
 
-    case 's':
-    case 'S':
-        stud_show(hptr);
-        break;
+            case 'm':
+            case 'M':
+                stud_mod(&hptr);
+                break;
 
-    case 'm':
-    case 'M':
-        stud_mod(&hptr);
-        break;
+            case 'v':
+            case 'V':
+                stud_save(hptr);
+                break;
 
-    case 'v':
-    case 'V':
-        stud_save(hptr);
-        break;
+            case 'e':
+            case 'E':
+                stud_exit(hptr);
+                break;
 
-    case 'e':
-    case 'E':
-        stud_exit(hptr);
-        break;
+            case 't':
+            case 'T':
+                stud_sort(&hptr);   
+                break;
 
-    case 't':
-    case 'T':
-        stud_sort(hptr);   
-       break;
+            case 'l':
+            case 'L':
+                stud_del_all_rec(&hptr);
+                break;
 
-    case 'l':
-    case 'L':
-        stud_del_all_rec(&hptr);
-        break;
+            case 'r':
+            case 'R':
+                stud_rev(&hptr);
+                break;
 
-    case 'r':
-    case 'R':
-        stud_rev(&hptr);
-        break;
+            default:
+                printf("invalid operation\n");
+                break;
+        }
 
-    default:
-        printf("invalid operation\n");
-        break;
-   }
-
+    } while(1);
 }
-   while(1);
-  }
-
 
 
