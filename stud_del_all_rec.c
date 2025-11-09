ed9f83d247312f732843f0519bdb6de552a936d0
Jabikza Jaya Swvan R <157294475+Jabikza05@users.noreply.github.com>
2025-11-09 17:51:45 +0530

    Update stud_del_all_rec.c

--- stud_del_all_rec.c
+++ stud_del_all_rec.c
@@ -2,12 +2,22 @@
 void stud_del_all_rec(ST **p)
 {
 	ST *temp;
-
+   char op;
+	
+	printf("Do you want to delete all the Records....[Y/y]:");
+	scanf(" %c",&op);
+	if(op=='Y'||op=='y')
+	{
 	while(*p!=0)
 	{
 		temp=*p;
 		*p=temp->next;
-	}
+		free(temp);
+	 }
 		printf("deleted all the students list...\n");
+    }
+    else
+    {
+	    printf("Records are not deleted....");
+    }
 }
-
